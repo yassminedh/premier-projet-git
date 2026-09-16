3a148374dd19cdc5efa203f296ebe0f4951fbac2
yassminederham <yassmineeducation1@gmail.com>
2026-09-16 17:24:24 +0000

    java script

--- hello.js
+++ hello.js
@@ -1,2 +1,5 @@
-console.log(Hello GitHup);
-cat hello.js
+const prompt=require("prompt-sync")();
+const name=prompt ("what is your name?");
+const color = prompt("what is your favorite color?");
+console.log(`Hello ${name}! Your favorite color is ${color}.`);
+
