ea39b1fce1e48e0bde6bc2f5f23462f81e9699b9
yassmineee <yassmineeducation1@gmail.com>
2026-09-16 22:02:31 +0000

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,4 @@
+const prompt = require ('prompt-sync') ();
+const name = prompt ("What is your name? ");
+const age = prompt ("How old are you? ");
+console.log(`Hello ${name}! You are ${age} years old.`);
