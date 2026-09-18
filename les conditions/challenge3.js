1a998b3f4318e90fb0590ed3454a44387e904f16
yassmineder <yassminederham1@gmail.com>
2026-09-18 11:10:36 +0000

    update

--- les conditions/challenge3.js
+++ les conditions/challenge3.js
@@ -1,11 +1,13 @@
-const prompt =require("prompt-sync")();
-let nombre1 = Number(prompt("entrer la valeur de nombre 1 :"));
-let nombre2 = Number(prompt ("entrer la valeur de nombre 2 :"));
+const prompt = require("prompt-sync")();
+let nombre1 = Number(prompt("entrer la valeur de nombre 1 : "));
+let nombre2 = Number(prompt ("entrer la valeur de nombre 2 : "));
 let somme = nombre1 + nombre2;
-if(nombre1===nombre2){
-    somme=somme*3;
-    console.log(somme);
+if(nombre1===nombre2) {
+    somme = somme * 3;
 }
+    console.log(somme);
+
+
 
 
 
